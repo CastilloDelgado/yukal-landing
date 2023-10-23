9184858e90ae53c237bb79ff31cc0170ddcd4ed2
Marco Castillo <marcocastillo1997@gmail.com>
2023-10-22 21:14:06 -0600

    Updating color in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,18 +10,12 @@ const options = ref([
         alt: "inicio",
         link: "/"
     },
-    // {
-    //     id: 2,
-    //     title: "Contacto",
-    //     alt: "contacto",
-    //     link: "/contacto"
-    // },
 ])
 
 </script>
 
 <template>
-    <nav class="bg-gradient-to-r from-primary-400 to-primary-300 fixed w-full">
+    <nav class="bg-gradient-to-r from-primary-700 to-primary-500 fixed w-full">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="relative flex h-16 items-center justify-between">
             <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -33,7 +27,7 @@ const options = ref([
                 <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                     <div class="hidden sm:ml-6 sm:block">
                         <div class="flex space-x-4">
-                            <a v-for="option in options" :key="option.id" :href="option.link" class="bg-primary-400/70 hover:bg-primary-400/100 hover:scale-105 transition text-light-100 rounded-md px-3 py-2 text-sm font-medium" aria-current="page">
+                            <a v-for="option in options" :key="option.id" :href="option.link" class="font-bold bg-primary-400/70 hover:bg-primary-400/100 hover:scale-105 transition text-light-100 rounded-md px-3 py-2 text-sm font-medium" aria-current="page">
                                 {{ option.title  }}
                             </a>
                         </div>
