2381d68aaeec3e266895c17b47cea78f85d05fd8
Marco Castillo <marcocastillo1997@gmail.com>
2023-10-26 23:14:54 -0600

    Ignoring large files in assets

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -35,19 +35,18 @@ const options = ref([
     <nav class="bg-primary-900 fixed w-full">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="relative flex h-16 items-center justify-between">
-            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div class="flex flex-shrink-0 items-center">
-                    <img class="h-8 w-auto" :src="logo" alt="Yu-Kal Logo">
-                    <img class="h-8 w-auto" :src="textLogo" alt="Yu-Kal Logo">
+                <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                    <div class="flex flex-shrink-0 items-center">
+                        <img class="h-8 w-auto" :src="logo" alt="Yu-Kal Logo">
+                        <img class="h-8 w-auto" :src="textLogo" alt="Yu-Kal Logo">
+                    </div>
                 </div>
-            </div>
                 <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                     <div class="align-middle hidden sm:flex">
                         <NavbarLink v-for="option in options" :key="option.id" :option="option"  />
                         <CustomButton class="text-sm bg-primary-600 hover:bg-primary-600 text-primary-100" title="Newsletter" color="light"/>
                     </div>
                 </div>
-                
             </div>
         </div>
     </nav>
