db3ba6a002193cafb91b079dc6259ae1e1277ff5
Marco Castillo <marcocastillo1997@gmail.com>
2023-10-30 16:43:30 -0600

    Resize image in product badge

--- src/components/ProductBadge.vue
+++ src/components/ProductBadge.vue
@@ -12,7 +12,7 @@ defineProps({
 <template>
     <div class="rounded-lg text-center p-3 w-full sm:w-1/2 lg:w-1/4 hover:scale-[102%] transition">
         <div class="w-full object-cover flex justify-center mb-6">
-            <img class="rounded-lg" :src="product.image" />
+            <img class="rounded-lg h-80 w-full object-cover" :src="product.image" />
         </div>
         <div class="px-4">
             <p class="mb-2 text-primary-600 font-bold text-2xl">{{ product.title }}</p>
--- src/components/VideoPresentation.vue
+++ src/components/VideoPresentation.vue
@@ -18,9 +18,9 @@ defineProps({
                 <div class="lg:w-1/2"> 
                     <div class="w-full h-full grid place-content-center">
                         <div class="py-16 lg:py-2 px-12">
-                            <p class="text-4xl text-center font-bold text-secondary-600 mb-4">Title</p>
-                            <p class="text-justify text-lg text-secondary-800">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus perspiciatis aliquid illum facere asperiores dolorum cupiditate quos corporis, omnis rerum eligendi commodi quasi, libero enim quam sed dolore. Rerum, vero? Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta iste nemo eaque quis modi ullam non, aspernatur delectus. Et obcaecati facilis a quae consequatur fuga aliquam doloribus suscipit quidem ipsam?</p>
-                            <div class="w-full flex justify-center mt-4">
+                            <p class="text-4xl text-center font-bold text-secondary-600 mb-4">Despertar de la conciencia</p>
+                            <p class="text-justify text-lg text-secondary-800 mb-2">El viaje del ser humano es el despertar de conciencia, el cual nos puede llegar a temprana edad o incluso antes de trasmutar. ¡Cada individuo viene a madre tierra con un propósito o misión, muchos de nosotros no lo sabemos o no lo recordamos!</p>
+                            <div class="w-full flex justify-center mt-8">
                                 <CustomButton title="Más información" class="bg-secondary-600 hover:bg-secondary-700 text-secondary-100" />
                             </div>
                         </div>
